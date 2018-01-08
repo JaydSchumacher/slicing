7350b55061e7546dd68fae2a98d48010d124518b
Jayd Schumacher <jaydschumacher@gmail.com>
2018-01-08 09:48:52 -0800

    Initial Commit

---
+++ slicing.py
@@ -0,0 +1,13 @@
+favorite_things = ['raindrops on roses', 'whiskers on kittens', 'bright copper kettles',
+                   'warm woolen mittens', 'bright paper packages tied up with string',
+                   'cream colored ponies', 'crisp apple strudels']
+slice1 = favorite_things[1:4]
+slice2 = favorite_things[5:]
+slice3 = favorite_things[:]
+slice3.sort()
+
+
+print(slice1)
+print(slice2)
+print(slice3)
+print(favorite_things)
